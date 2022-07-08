deef393dbb194a7b33ef42da5dfee5d4532f87bf
Chuck Norris <briandouglas@abc.com>
2022-07-08 15:34:36 +0100

    cocker spaniel

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -10,33 +10,30 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
+  i = 0;
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    var images = data.message;
+    images.forEach(image => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
       const h1 = document.createElement('h1')
-      h1.textContent = movie.title
-
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
+      h1.textContent = "Cocker"
 
       const img = document.createElement('img')
-      img.src = movie.image;
+      img.src = image;
 
       container.appendChild(card)
       card.appendChild(h1)
-      card.appendChild(p)
       card.appendChild(img)
-      
     })
   } else {
     const errorMessage = document.createElement('marquee')
+
     errorMessage.textContent = `Gah, it's not working!`
     app.appendChild(errorMessage)
   }
